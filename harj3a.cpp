5829446975f5b5c18ff519461e7f183a1f6de216
ollikauppinen <olli_kauppinen@luukku.com>
2013-10-14 16:37:33 +0300

    Palautus

---
+++ harj3a.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void main (void)
+	{
+		cout << "Olli\nKauppinen\nMyllylammenkatu 7 A 9\n41160 Tikkakoski\n";
+	}
---
+++ harj3b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void main (void)
+	{
+		cout << "**********************************\n";
+		cout << "* *\n";
+		cout << "* Olli *\n";
+		cout << "* Kauppinen *\n";
+		cout << "* *\n";
+		cout << "*-------------------------------------- *\n";
+		cout << "* *\n";
+		cout << "* Myllylammenkatu 7 A 9 *\n";
+		cout << "* 41160 Tikkakoski *\n";
+		cout << "* *\n";
+		cout << "**********************************\n";
+	}
